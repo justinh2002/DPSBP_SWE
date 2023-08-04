77f33bae654d38860a9d23fa4dcadca78e9c9d21
Justin Kin Jun Hew <justinhew@Justins-MacBook-Air.local>
2023-08-04 17:55:53 +1000

    added 2D cases

---
+++ SWE_2d/RK4.py
@@ -0,0 +1,51 @@
+import numpy as np 
+import SWESBP_2d
+    
+def acoustic_RK4(self,ru, rv, rp,u, v, p, rho, K, nx,ny, dx,dy, order, x, y, t, dt, type_0, Ubar,Vbar, H, g,flux_type,vorticity):
+
+        # fourth order Runge-Kutta time-stepping
+
+
+
+        # intialize arrays for Runge-Kutta stages
+        k1u = np.zeros((nx, ny))
+        k1v = np.zeros((nx, ny))
+        k1p = np.zeros((nx, ny))
+        
+        k2u = np.zeros((nx, ny))
+        k2v = np.zeros((nx, ny))
+        k2p = np.zeros((nx, ny))
+        
+        k3u = np.zeros((nx, ny))
+        k3v = np.zeros((nx, ny))
+        k3p = np.zeros((nx, ny))
+        
+        k4u = np.zeros((nx, ny))
+        k4v = np.zeros((nx, ny))
+        k4p = np.zeros((nx, ny))
+        
+        
+        
+
+
+        self.acoustic_rate(k1u,k1v, k1p,u, v, p, rho, K, nx,ny, dx,dy, order, t,x, y,  type_0, Ubar,Vbar,H,g,flux_type,vorticity)
+       # def acoustic_rate(self,hu,hv, hp,u, v, h, rho, K, nx, ny,dx,dy, order, t, x,y, type_0, Ubar,Vbar,H, g):
+        
+
+        self.acoustic_rate(k2u,k2v, k2p,u + 0.5*dt*k1u, v+0.5*dt*k1v, p+0.5*dt*k1p, rho, K, nx,ny, dx,dy, order, t+0.5*dt,x, y,  type_0, Ubar,Vbar, H, g,flux_type,vorticity)
+
+        self.acoustic_rate(k3u,k3v, k3p, u + 0.5*dt*k2u ,v+0.5*dt*k2v, p+0.5*dt*k2p, rho, K, nx,ny, dx, dy,order, t+0.5*dt, x, y, type_0, Ubar,Vbar, H,g,flux_type,vorticity)
+
+
+
+        self.acoustic_rate(k4u,k4v, k4p,u+dt*k3u, v+dt*k3v, p+dt*k3p, rho, K, nx,ny, dx,dy, order, t+dt,x, y, type_0, Ubar,Vbar, H,g,flux_type,vorticity)
+
+        # update fields
+        ru[:,:] = u + (dt/6.0)*(k1u + 2.0*k2u + 2.0*k3u + k4u)  
+        rv[:,:] = v + (dt/6.0)*(k1v + 2.0*k2v + 2.0*k3v + k4v)  
+        rp[:,:] = p + (dt/6.0)*(k1p + 2.0*k2p + 2.0*k3p + k4p)
+
+
+
+
+            
---
+++ SWE_2d/SWESBP_2d.py
@@ -0,0 +1,262 @@
+import numpy as np
+
+class SWE_SBP:
+    
+    def __init__(self):
+
+        self.ndim     = 1 # the dimensionality (1,2)
+        self.ncells   = np.array([500]) # number of cells (2D)
+        self.scheme   = "sbp" # sbp
+        self.boundary = "non-periodic" # boundary condition
+        self.cfl      = 0.8 # CFL safety factor
+        self.nend     = 100 # number of timesteps
+        self.tmax     = 1e10 # end time
+        self.dx       = 1.0 # cell size x
+        self.dy       = 1.0 # cell size y
+        
+    def mms(self,U,V, P, U_t, V_t, P_t, U_x,V_x, P_x,x, y, t, Ubar,Vbar,type_0,nx,ny,dy,dx,order):
+        from sbp_operators_periodic import dxd_m_SBP_periodic,dyd_m_SBP_periodic
+
+        import numpy as np
+
+
+        if type_0 in ('Gaussian'):
+
+            delta = (y[-1,0]-y[0,0])
+
+            rho = 1#2.6702     # density [g/cm^3]
+            K =  2.2 #rho*cs**2 
+            #cs = np.sqrt(K/rho)# shear modulus [GPa]
+            #Zs = rho * cs
+            
+            g = 9.81
+            H = 10
+            
+            cs =np.sqrt(g*H) #Ubar np.sqrt(g*H)
+
+            y0 = 0.5*(y[0, -1]-y[0, 0])
+            x0 = 0.5*(x[-1, 0]-x[0, 0])
+            
+
+            V[:,:]=  (np.exp(-((y -y0)**2 +(x - x0 )**2-  cs*t)**2)/1)
+            U[:,:] =  (np.exp(-((y -y0)**2 +(x - x0 )**2-  cs*t)**2)/1)
+            #*p.cos(2*np.pi*y) #1/np.sqrt(2.0*np.pi*delta**2)*0.5*(np.exp(-(y+cs*(t)-x0)**2/(2.0*delta**2))\
+            U_t[:,:]   =2* cs* V*(x -x0 -  cs*t)/1                          #   + np.exp(-(y-cs*(t)-x0)**2/(2.0*delta**2)))
+
+            P[:,:] =   V + 10 #(np.exp(-(y - cs*(t))**2/(0.3 * x0)))#*np.cos(2*np.pi*y) #0 #1/np.sqrt(2.0*np.pi*delta**2)*0.5*Zs*(np.exp(-(y+cs*(t)-x0)**2/(2.0*delta**2))\
+                                                 #   - np.exp(-(y-cs*(t)-x0)**2/(2.0*delta**2)))
+
+            V_t[:,:] =   2* cs* V*(y -x0 -  cs*t)/1# 10/x0* (-cs/0.3) * 2 * t  * (np.exp(-( cs*(t))**2/(0.3)))*np.cos(2*np.pi*y)
+            P_t[:,:] =  V_t
+            
+            U_x =-U*2*(x -x0 -  cs*t)/1 # 5/x0 *(-cs/0.3) * 2 * t  * (np.exp(-(cs*(t))**2/(0.3)))*np.cos(2*np.pi*y)
+            V_x[:,:] =  -V*2*(y -x0 -  cs*t)/1 #10 * (-2*np.pi) * (np.exp(-( cs*(t))**2/(0.3)))*np.sin(2*np.pi*y)
+            P_x[:,:] =  V_x#
+            
+        elif type_0 in ('KH'):
+            # domain size is [-xlim, xlim] x [-ylim, ylim]
+            xlim = 20_000_000
+            ylim = 20_000_000
+            # centre shear here
+            yc = 10_000_000
+            # atmosphere-ish parameters
+            f = 2 * 7.292e-5
+            H = 10_000
+            g = 10
+            # initial max windspeed
+            mag = 50.0
+            wv = 1 / 1_000_000
+
+            #initial condition:
+
+            #def initial_condition(xs, ys, mag=1.0):
+            def sech(x):
+                return 1 / np.cosh(x)
+
+            neg_mask = (y < 0).astype(y.dtype)
+            pos_mask = (y > 0).astype(y.dtype)
+            U = -pos_mask * mag * sech(wv * (y +4e4- yc)) ** 2
+            U += neg_mask * mag * sech(wv * (y +2e4 + yc)) ** 2
+
+            P = pos_mask * (mag / wv) * np.tanh(wv * (y - yc)) * (f / g)
+            P += -neg_mask * (mag / wv) * np.tanh(wv * (y + yc)) * (f / g)
+            #print(h.max(), h.min())
+            P += H
+            V = np.zeros_like(U)
+
+                
+            
+            # g = 9.81
+            # mask_hl= np.logical_and(0< y, y < 5)
+            # mask_hr = np.logical_and(5< y, y < 10)
+            # psi = np.zeros((nx,ny))
+            # psi_x = np.zeros((nx,ny))
+            # psi_y = np.zeros((nx,ny))
+            
+            
+            
+
+            # #gradient_h = 
+            # #b[mask_b] = 0.2 - 0.05*(y[mask_b] - 10 )**2
+            # f = 2 * 7.292e-5
+            
+            # for i in np.arange(nx):
+            #     for j in np.arange(ny):
+            #         if y[i,j] >= 2e7:
+            #             P[i,j] = 10**4 + 50 *f/(1e-6*g) * 1/np.cosh(1e-6 * (y[i,j]-3e7))
+            #         elif y[i,j] < 2e7:
+            #             P[i,j] = 10**4  -50 *f/(1e-6*g) * 1/np.cosh(1e-6 * (y[i,j]- 1e7))  
+            # psi = -g/f * P         
+            # dyd_m_SBP_periodic(psi_y, psi,ny,dy,order)
+            # dxd_m_SBP_periodic(psi_x,psi, nx,dx,order)
+            # U = psi_y 
+            # V = -psi_x
+                
+    def acoustic_rate(self,hu,hv, hp,u, v, h, rho, K, nx, ny,dx,dy, order, t, x,y, type_0, Ubar,Vbar,H, g,flux_type,vorticity):
+    # we compute rates that will be used for Runge-Kutta time-stepping
+    # # 
+    #         from dpsbp_operators_correct import dxd_m_DP, dxd_p_DP
+    #         from DRP_FD import dxd_m_DRP, dxd_p_DRP
+    #         from DRP_FD_periodic import dxd_m_DRP_periodic, dxd_p_DRP_periodic
+    #         from dpsbp_operators_periodic import dxd_m_DP_periodic, dxd_p_DP_periodic
+            from sbp_operators_periodic import dxd_m_SBP_periodic,dyd_m_SBP_periodic
+            # from sbp_operators import dxd_m_SBP
+
+            import numpy as np
+        
+            #delta = (y[-1,0]-y[0,0])
+        # L = 10
+            #ubar = 0
+            #H = 1
+            #g = 9.8
+            #ubar= 0.5*np.sqrt(g*H)
+
+
+            U = np.zeros((nx, ny))
+            V = np.zeros((nx, ny))
+            P = np.zeros((nx, ny))
+            
+            Ut = np.zeros((nx, ny))
+            Vt =  np.zeros((nx, ny))
+            Pt = np.zeros((nx, ny))
+            
+            Ux = np.zeros((nx, ny))
+            Vx = np.zeros((nx, ny))
+            Px = np.zeros((nx, ny))
+            
+
+            uy = np.zeros((nx,ny))
+            vx = np.zeros((nx,ny))
+            
+            
+                    
+            bx = np.zeros((nx, ny))
+            
+            b = np.zeros((nx, ny))
+            # if topography == 'non_smooth':
+            #     b = 0 * y
+            #     bx = 0 * y
+            #     mask_b = np.logical_and(8 < y, y < 12)
+            #     b[mask_b] = 0.2 - 0.05*(y[mask_b] - 10 )**2 
+                
+            #     bx[mask_b] = - 0.10 * (y[mask_b] - 10 )
+            #     #mask_b = np.logical_and(8 < y, y < 12)
+            
+            #     #mask_a = np.logical_and(12 <= y, y <= 8)
+
+            # elif topography == 'smooth':
+            #     b = 0 * y
+            #     bx = 0 * y
+            #     b = 0.1 * np.exp( -(y-10)**2/0.3) 
+            #     bx = -2*b* ( y - 10) / 0.3
+                
+            # else:
+            #     bx = 0.0
+            #     b = 0.
+            
+            self.mms(U, V,P, Ut,Vt,Pt, Ux,Vx, Px,x, y, t,Ubar,Vbar, type_0,nx,ny,dx,dy,order)
+
+            if flux_type == 'linear':
+                flux_hx = Ubar*(h)+ H*u
+                flux_hy = Vbar*(h) +H*v  #h
+                flux_u =  (Ubar*u+Vbar*v) + g*(h) ##Ubar*v + g*h
+
+            #             flux_1 = Ubar*v + g*h
+            # flux_2 = Ubar*h + H*v
+            
+            if flux_type == 'nonlinear':
+                #b = 0
+                flux_hx = u*(h)
+                flux_hy = v*(h)  #h
+                flux_u =  (u**2+v**2)/2 + g*(h) ##Ubar*v + g*h
+                
+                #flux_v = (u**2 + v**2)/2 + g*(h+b)
+            # initialize arrays for computing derivatives
+
+                
+            flux_hx_x = np.zeros((nx,ny))
+            
+            
+            flux_hy_y = np.zeros((nx,ny))
+            flux_u_x = np.zeros((nx,ny))
+            flux_u_y =np.zeros((nx,ny))
+            
+            # if fd_type == 'DP':
+            #     dxd_m = dxd_m_DP
+            #     dxd_p = dxd_p_DP
+                
+            # if fd_type == 'DRP':
+            #     dxd_m = dxd_m_DRP
+            #     dxd_p = dxd_p_DRP
+
+            # if fd_type == 'SBP':
+            #     dxd_m = dxd_m_SBP
+            #     dxd_p = dxd_m
+            
+            # if fd_type == 'DRP_periodic':
+            #     dxd_m = dxd_m_DRP_periodic
+            #     dxd_p = dxd_p_DRP_periodic
+                
+            # if fd_type == 'DP_periodic':
+            #     dxd_m = dxd_m_DP_periodic
+            #     dxd_p = dxd_p_DP_periodic
+            
+           # if fd_type == 'SBP_periodic':
+            dxd_m = dxd_m_SBP_periodic
+            dxd_p = dxd_m
+            dyd_m = dyd_m_SBP_periodic
+            
+
+            
+            
+            dxd_m(flux_hx_x, flux_hx, nx,dx, order)
+            dyd_m(flux_hy_y, flux_hy, ny,dy, order)
+            dxd_m(flux_u_x, flux_u, nx, dx, order)
+            dyd_m(flux_u_y, flux_u,  ny,dy, order)
+            
+              
+            
+                  
+            
+                    
+            hu[:,:] = -flux_u_x
+            hv[:,:] = -flux_u_y    #- g * 0 * bx
+            hp[:,:] = -flux_hx_x - flux_hy_y 
+            
+            
+            
+            if vorticity == 'true':
+                f = 2 * 7.292e-5
+                
+                dyd_m(uy, u,ny,dy,order)
+                dxd_m(vx,v, nx,dx,order)
+                
+                vort = (uy - vx + f) 
+                hu = hu + vort * v
+                hv = hv - vort *  u
+                
+        
+            
+
+    
+    
